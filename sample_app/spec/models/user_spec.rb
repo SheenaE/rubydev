f9efd8211891163f9ade7b046fc2c843f72ce8d4
Sheena Ellenburg <sheena.ellenburg@oregonstate.edu>
2013-06-28 14:23:14 -0700

    Finish user signup

--- sample_app/spec/models/user_spec.rb
+++ sample_app/spec/models/user_spec.rb
@@ -14,7 +14,6 @@ require 'spec_helper'
 describe User do
   
   before do
-    #@user = User.new(name: "Example User", email: "user@example.com")
     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
   
@@ -38,6 +37,16 @@ describe User do
     before { @user.email = " " }
     it { should_not be_valid }
   end
+
+  describe "when email address is mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM"}
+    
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end  
 
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
